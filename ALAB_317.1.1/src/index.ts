cae2ee2d10c31e7d0e1c78e336d2c0f1c0966dd1
Kevin Dang <kevin.kd.dang@gmail.com>
2024-09-05 09:45:00 -0400

    ALAB 317.1.1

---
+++ ALAB_317.1.1/src/index.ts
@@ -0,0 +1,62 @@
+interface Vehicle {
+  make:string,
+  model:string,
+  wheels:number
+}
+
+class Vehicle {
+    status:string = "stopped";
+    constructor(make:string, model:string, wheels:number) {
+      this.make = make;
+      this.model = model;
+      this.wheels = wheels;
+    }
+    start() {
+      this.status= "started";
+    }
+    stop() {
+      this.status = "stopped";
+    }
+  }
+  
+  class Car extends Vehicle {
+    constructor(make:string, model:string) {
+      super(make, model, 4);
+    }
+  }
+  
+  class MotorCycle extends Vehicle {
+    constructor(make:string, model:string) {
+      super(make, model, 2);
+    }
+  }
+
+  class NCycle<T>{
+    make:T|T[];
+    model:T|T[];
+    constructor(make:T|T[], model:T|T[]){
+        this.make = make;
+        this.model = model;
+    }
+    print(){
+      
+    }
+  }
+  
+  function printStatus(vehicle:Vehicle) {
+    if (vehicle.status === "running") {
+      console.log("The vehicle is running.");
+    } else {
+      console.log("The vehicle is stopped.");
+    }
+  }
+  
+  const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
+  myHarley.start();
+  printStatus(myHarley);
+  console.log(myHarley.make.toUpperCase());
+  
+  const myBuick = new Car("Buick", "Regal");
+  myBuick.wheels = myBuick.wheels - 1;
+  console.log(myBuick.wheels);
+  console.log(myBuick.model);
